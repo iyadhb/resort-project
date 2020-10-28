619491095abdf8f9ddd71bdb7eb1730568469467
Iyad <iyadhb@gmail.com>
2020-10-28 20:34:21 +0000

    Hero Component

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Hero from '../globals/Hero'
+import aboutImg from '../../images/aboutBcg.jpeg'
 
 const Header = () => {
     return (
-        <div>
-          Hello from header  
-        </div>
+       <> 
+      <Hero img={aboutImg}>
+        <h1>Hello From the Resort</h1>
+        </Hero>
+        </>
     )
 }
 
