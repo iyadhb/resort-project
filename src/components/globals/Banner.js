351793c905f8aab83b1c04734330fc76eb009a5e
Iyad <iyadhb@gmail.com>
2020-10-29 14:27:56 +0000

    setup banner component

---
+++ src/components/globals/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled, {css, keyframes} from 'styled-components'
+import {setColor} from '../../styles'
+
+const Banner = ({ className ,title, text, children, greeting }) => {
+    return (
+        <div className={className}>
+
+            <h1>{greeting} <span>{title}</span></h1>
+            <div className="info"></div>
+            <p>{text}</p>
+            {children}
+        </div>
+    )
+}
+
+const BannerWrapper = styled(Banner)`
+color: red;
+
+`
+export default BannerWrapper;
--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../globals/Hero'
 import homeImg from '../../images/homeBcg.jpeg'
+import Banner from '../globals/Banner'
 
 const Header = () => {
     return (
        <> 
       <Hero img={homeImg}>
-        <h1>Hello From the Resort</h1>
+        <Banner greeting="Welcome to" title="beachwalk resort" text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea corrupti recusandae fugiat harum cum distinctio quis voluptates praesentium vel tenetur. Provident, ad inventore ullam quos ut facere et sint magni." />
+      
         </Hero>
         </>
     )
