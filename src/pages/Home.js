c7e825cd2f2ccbcb46355e15461b03f0c2f89958
Iyad <iyadhb@gmail.com>
2020-10-30 20:46:00 +0000

    Section Component and Title Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import About from '../components/home/About'
 import Header from '../components/home/Header'
 
 const Home = () => {
     return (
         <>
            <Header /> 
+           <About />
         </>
     )
 }
